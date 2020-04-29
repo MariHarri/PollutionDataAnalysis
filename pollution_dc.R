33169cf634d70c124bba50f192bc23d09983e35b
MariHarri <chrismarieharrington@yahoo.com>
2020-04-29 13:11:39 -0500

    Cleaned up the code a bit, changed some graphical parameters (fixing the cex statement to do what was intended)

--- pollution_dc.R
+++ pollution_dc.R
@@ -1,40 +1,37 @@
 rm(list=ls())
 library(stringr)
-
+#recording directory name
 specdata<-'C:/Users/chris/OneDrive/Desktop/School Docs/Coursera Assignments/specdata'
 
-par(mfrow=c(1,2),mar=c(5,5,5,5),bg='light gray',lwd=2)
-pollution_dc<-function(directory,id=1:332,threshold=0){
+pollution_dc<-function(directory,id,threshold=0){
+    #changing some graphical parameters for visibility
+    par(mfrow=c(1,2),mar=c(5,5,5,5),bg='light gray',lwd=2)
+    #initiating variables with empty vectors
     sulfate.mean<-c()
     nitrate.mean<-c()
     id.list<-c()
+    cex<-1
     for(i in id){
-        #used str_pad to deal with the leading zeros in the id name
+        #used str_pad to deal with the leading zeros in the file names
         x<-str_pad(i,3,pad='0')
         filename<-paste0(directory,'/',x,'.csv')
         file<-read.csv(filename)
-        #this determines if the NA threshold level is reached
+        #this if/else determines if the NA threshold level is reached
         nas.removed<-complete.cases(file)
         z<-file[nas.removed,]
         if(nrow(z)>threshold){
             sulfate.mean<-c(sulfate.mean,mean(z$sulfate))
             nitrate.mean<-c(nitrate.mean,mean(z$nitrate))
-            id.list<-c(id.list,i)
-        }
-        else{print(paste('ID #',i,' has less than the maximum allowable NAs',sep=''))}
-    }
-    if(length(id)<10){
-        print('Recommended:use at least 10 ID#s')
-    }
-    if(length(id)>40&length(id)<40){
-        print('Recommended:use less than 40 ID#s')
-        par(cex=.7)
-    }
-    dotchart(sulfate.mean,main='Sulfate Levels by Location',col='blue',pch=16,
-        lcolor='dimgray',xlab='Mean Level of Sulfate',labels=id.list,ylab='Monitor IDs')
-    dotchart(nitrate.mean,main='Nitrate Levels by Location', col='red',pch=16,
-        lcolor='dimgray',xlab='Mean Level of Nitrate',labels=id.list,ylab='Monitor IDs')
-
+            id.list<-c(id.list,i)}
+        else{print(paste('ID #',i,' has less than the maximum allowable NAs',sep=''))}}
+    #if statements dealing with scaling and different quantities of location IDs
+    if(length(id)>20){cex<-.7}
+    if(length(id)>40){print('Recommended:use no more than 40 ID#s to ensure data points are distinct')}
+    #statements for producing the two charts
+    dotchart(sulfate.mean,col='blue',pch=16, lcolor='dimgray',labels=id.list,cex=cex)
+    title('Sulfate Levels by Location',xlab='Mean Level of Sulfate',ylab='Monitor IDs')
+    dotchart(nitrate.mean,col='red',pch=16, lcolor='dimgray',labels=id.list,cex=cex)
+    title('Nitrate Levels by Location',xlab='Mean Level of Nitrate',ylab='Monitor IDs')
 }
 
-pollution_dc(specdata,id=1:39,threshold=100)
+pollution_dc(specdata,id=1:20,threshold=100)
